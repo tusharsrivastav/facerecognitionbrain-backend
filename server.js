3750c1a0fb2570544cd0fa03d64e289b5cef5585
Tushar Srivastava <tusharsrivastava@Tushars-MacBook-Air.local>
2024-07-22 16:06:39 +0530

    prepare env variables for production

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import bcrypt from "bcrypt";
 import knex from "knex";
+import * as dotenv from "dotenv";
+
+dotenv.config({
+  path: "./env",
+});
 
 // CONTROLLERS
 import register from "./controllers/register.js";
@@ -11,29 +16,50 @@ import getProfile from "./controllers/profile.js";
 import image from "./controllers/image.js";
 
 // CONNECT TO POSTGRES DATABASE
+// const db = knex({
+//   client: "pg",
+//   connection: {
+//     host: "127.0.0.1",
+//     user: "postgres",
+//     password: "",
+//     database: "smart-brain",
+//   },
+// });
+
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "",
-    database: "smart-brain",
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    port: process.env.PGPORT,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
   },
 });
 
 const app = express();
-// const PORT = 3001;
+const PORT = process.env.PORT;
 
 // MIDDLEWARES
 app.use(bodyParser.json());
 app.use(cors());
 
 // API ENDPOINTS
-app.get("/", (req, res) => { res.send("success") });
-app.post("/signin", (req, res) => { signin(req, res, db, bcrypt) });
-app.post("/register",  (req, res) => { register(req, res, db, bcrypt) });
-app.get("/profile/:id", (req, res) => { getProfile(req, res, db) });
-app.put("/image", (req, res) => { image(req, res, db) });
+app.get("/", (req, res) => {
+  res.send("success");
+});
+app.post("/signin", (req, res) => {
+  signin(req, res, db, bcrypt);
+});
+app.post("/register", (req, res) => {
+  register(req, res, db, bcrypt);
+});
+app.get("/profile/:id", (req, res) => {
+  getProfile(req, res, db);
+});
+app.put("/image", (req, res) => {
+  image(req, res, db);
+});
 
 // STARTING THE SERVER
 app.listen(PORT, () => {
